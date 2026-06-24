6a04f0a77f32fde57c540ae6cd4b353246aa2752
ramy1777 <63922993+ramy1777@users.noreply.github.com>
2026-06-24 18:42:29 +0000

    Fix capacitor config

---
+++ android/app/src/main/java/com/glassify/worldcup/MainActivity.java
@@ -0,0 +1,5 @@
+package com.glassify.worldcup;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ android/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ramy.worldcup',
+  appName: 'Coupe du Monde',
+  webDir: 'dist'
+};
+
+export default config;
